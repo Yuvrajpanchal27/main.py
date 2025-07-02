1ca3aa2b5091a04f6c2b1bbdf25a1468dc26e52f
Yuvrajpanchal27 <panchalyuvraj126@gmail.com>
2025-07-02 12:27:11 +0530

    Add files via upload

---
+++ Task_1.py
@@ -0,0 +1,12 @@
+num1 = int(input("Enter the frist number:"))
+num2 = int(input("Enter the second number:"))
+
+Addition = num1 + num2
+Subraction = num1 - num2
+Multilication = num1 * num2
+Division = num1 / num2
+
+print("Addition:",Addition)
+print("Subractiob:",Subraction)
+print("Multilication:",Multilication)
+print("Division:",Division)
---
+++ Task_2.py
@@ -0,0 +1,5 @@
+fristname = input("Enter the frist name:")
+secondname = input("Enter the second name:")
+
+fullname = fristname + " " + secondname
+print("Hello," +fullname+ "! Welcome to the python program")
